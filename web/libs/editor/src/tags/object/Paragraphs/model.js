951fe66afde1ee2b414e07502a0ce52f5f563421
jeet-1236 <claudedigitalnext@gmail.com>
2026-09-23 05:42:05 +0000

    ls-10-transcript-lines-reversed: planted frontend defect for the AgentForge lab

--- web/libs/editor/src/tags/object/Paragraphs/model.js
+++ web/libs/editor/src/tags/object/Paragraphs/model.js
@@ -663,7 +663,7 @@ const ParagraphsLoadingModel = types.model().actions((self) => ({
           const bEnd = b.end ? b.end : b.start + b.duration || 0;
 
           if (a.start === b.start) return aEnd - bEnd;
-          return a.start - b.start;
+          return b.start - a.start;
         })
       : val;
 
